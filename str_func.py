a0d3efd259b51dd4b55d7e1e641ab8d50d185110
Lim Dmitriy <lim79201307@gmail.com>
2024-02-20 02:23:06 +0900

    add upper_word func

--- str_func.py
+++ str_func.py
@@ -1,5 +1,12 @@
 def upper_string(input_string):
-    ###
+    """
     Преобразует строку в верхний регистр
-    ###
+    """
     return input_string.upper()
+
+def upper_word(input_word):
+    """
+    Делает заглавными первые буквы каждого слова в строке
+    """
+    return input_word.title()
+
